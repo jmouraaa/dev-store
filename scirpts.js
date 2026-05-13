039296df9f5fd7388c858fd397c9571afc49d036
jmouraaa <suporte@jmourazig@gmail.com>
2026-05-13 00:10:51 -0300

    Primeiro commit

--- scirpts.js
+++ scirpts.js
@@ -10,14 +10,47 @@ const total = items.length
 let timer;
 
 function update(direction){
+
     document.querySelector('.item.active').classList.remove('active')
     document.querySelector('.dot.active').classList.remove('active')
 
     if(direction > 0){
+
+        active = active + 1
         
+        if(active === total){
+            active = 0
+        }
+
     }
 
+    else if(direction < 0 ){
+
+        active = active - 1
+
+        if(active < 0){
+            active = total - 1
+        }
+
+    }
+
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
+
+
+    numberIndicator.textContent = String(active + 1).padStart(2,'0')
+
 }
+
+
+clearInterval(timer)
+timer = setInterval(() => {
+        update(1)
+    }, 5000);
+
+
+
+
 
 prevButton.addEventListener('click', () => { 
     update(-1)
